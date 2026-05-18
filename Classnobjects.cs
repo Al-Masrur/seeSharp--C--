563b6e2f1259ee522a05616ecd5220fe31acbc28
Al-Masrur <masrur2305341147@diu.edu.bd>
2026-05-18 19:59:54 +0600

    classobject

--- Classnobjects.cs
+++ Classnobjects.cs
@@ -1,11 +1,35 @@
 using System;
-class Car
-{
-    string color = "red";
 
-    static void Main(string[] args)
+namespace myApp
+{
+    //this is a class
+    class Car                   
     {
-        Car obj = new Car();
-        Console.WriteLine(obj.color);
+        //attributes of car class
+        public string color ="";         
+        public string model ="";
+
+    }
+
+    //this is another class
+    class Vehicle               
+    {
+        static void Main(string[] args)
+        {
+            Car toyota = new Car();            //obj of Car class inside Vehicle class
+            toyota.color = "red";
+            toyota.model = "crown";
+            Console.WriteLine("Car 1:\n"+ toyota.color);    //calling car class attributes
+            Console.WriteLine(toyota.model); 
+
+Console.WriteLine("");
+
+            Car lamborghini = new Car();            //obj of Car class inside Vehicle class
+            toyota.color = "yellow";
+            toyota.model = "Aventador";
+            Console.WriteLine("Car 2:\n"+toyota.color);    //calling car class attributes
+            Console.WriteLine(toyota.model);
+
+        }
     }
 }
